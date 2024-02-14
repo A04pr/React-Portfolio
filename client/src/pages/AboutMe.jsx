fbe87be5d8cc588f78452e87603688ff5995e939
A04pr <areed041704@gmail.com>
2024-02-13 18:11:19 -0700

    added about me and github/linkedin link

--- client/src/pages/AboutMe.jsx
+++ client/src/pages/AboutMe.jsx
@@ -4,8 +4,13 @@ export default function AboutMe() {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">About Me</h1>
       <p className="text-lg">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-        Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+        Welcome to my Portfolio!
+
+        My name is Aidan and i'm a student enrolled in the University of Utah full-stack coding bootcamp. This website contains a section about myself and my history, a section containing some of the projects ive worked on and have been a part of, and a finally a section containing contact information. Click the links in the navagation bar above to switch to a different page.
+
+        Throughout this course ive had the opportunity to work with and learn many components within 'web development', stuff like Javascript, HTML, CSS, and SQL. I think that all of the different components of web development are interesting and useful in their own ways, but I do have my favorites. I prefer workign with front end and UI, utilizing React and Vite as well as third party stylesheets such as Bootstrap or Tailwindcss. I would love to improve my skills on the front-end, and i'm sure I will, but I also realize the importance of the back-end too. I've worked extensively with SQL, Sequelize, Express, and small packages such as dotenv or jest. The back-end is where I believe I have the most room to improve, and that's where I intend to focus my efforts after the completion of this course.
+
+        As previously mentioned I am still enrolled in the coding bootcamp, so currently my focus is on graduating the bootcamp. After I gradutate though, I plan on looking for internships or entery level jobs in the field. I plan on travelling often so I intend to find some virtual or semi-virtual options for work. 
       </p>
     </div>
   );
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,8 +3,8 @@ export default function Contact() {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-4">Contact Information</h1>
         <div className="flex items-center space-x-4">
-          <a href="#" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600 transition duration-300">Github</a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:text-blue-600 transition duration-300">LinkedIn</a>
+          <a href="https://github.com/A04pr" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600 transition duration-300">Github</a>
+          <a href="www.linkedin.com/in/aidanpreed" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:text-blue-600 transition duration-300">LinkedIn</a>
           <p>Email: </p>
         </div>
       </div>
